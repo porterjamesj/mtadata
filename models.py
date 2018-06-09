cb7f1a6eb929bfc6e077600107aec8ceea625210
James Porter <james@jamesporter.me>
2018-06-09 19:07:33 -0400

    pipenv, cleanup a bit

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.dialects.postgresql import UUID, insert
 
-def start_db():
-    engine = create_engine('postgresql://localhost/subway_data')
+def start_db(conn_str):
+    engine = create_engine(conn_str)
     Session = sessionmaker(bind=engine)
     MTABase.prepare(engine)
     return engine, Session
